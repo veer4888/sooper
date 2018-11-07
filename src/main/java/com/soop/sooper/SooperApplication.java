ec19870b5c2981753d3b5996076fd43d1766bbdc
= <=>
2018-11-07 18:58:23 +0530

    Added default spring boot project skelton

---
+++ src/main/java/com/soop/sooper/SooperApplication.java
@@ -0,0 +1,12 @@
+package com.soop.sooper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SooperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SooperApplication.class, args);
+	}
+}
---
+++ src/test/java/com/soop/sooper/SooperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.soop.sooper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SooperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
